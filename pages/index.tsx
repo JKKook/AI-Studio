c388ba62fd755b2ece4c7e52fa0ab7ecb426d5a8
JKKook <whdrnrdldia@naver.com>
2023-07-28 12:05:59 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import App from '@/src/components/Templates/App';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <App />
+        </>
+    );
+};
+
+export default Home;
---
+++ pages/layout/layout.tsx
@@ -0,0 +1,16 @@
+// These styles apply to every route in the application
+import '../../../styles/global.css';
+import Home from '../index';
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang='ko'>
+            <Home />
+            {children}
+        </html>
+    );
+}
