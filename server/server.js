dad56fa0be2a0fd26bcc9134afe33306a8470067
JKKook <whdrnrdldia@naver.com>
2023-07-31 15:19:12 +0900

    Server open

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const openai = new OpenAIApi(configuration);
 const { Configuration, OpenAIApi } = require('openai');
 const express = require('express');
 const cors = require('cors');
-const PORT = 7003;
+const PORT = 7005;
 const app = express();
+require('dotenv').config();
 
 // POST 요청 처리
 app.use(express.urlencoded({ extended: true }));
@@ -16,44 +16,48 @@ let corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// Server
-app.get('/', function (req, res) {
-    // console.log('server open')
-});
+// // Server
+// app.get('/', function (req, res) {
+//     res.send(`server open`);
+// });
 
 // GPT Settings
 const configuration = new Configuration({
-    apiKey: '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',
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
+const openai = new OpenAIApi(configuration);
 
-app.post('/studio/chatgpt/answers', function (req, res) {
-    async function apiCall() {
-        const completion = await openai.createChatCompletion({
-            model: 'gpt-3.5-turbo',
-            max_tokens: '30',
-            temperature: 0.5,
-            messages: [
-                {
-                    role: 'system',
-                    content:
-                        '당신의 최고의 프론트엔드 개발자입니다. 만약 변수명에 대한 질문을 받는다면 해당 질문에 대한 올바른 답변을 할 수 있습니다 ',
-                },
-                {
-                    role: 'user',
-                    content:
-                        '당신의 최고의 프론트엔드 개발자입니다. 만약 변수명에 대한 질문을 받는다면 해당 질문에 대한 올바른 답변을 할 수 있습니다 ',
-                },
-                { role: 'assistant', content: '안녕하세요 CodrLee 입니다' },
-                { role: 'user', content: '' },
-            ],
-        });
-
-        const answer = completion.data.choices[0].message['content'];
-        console.log(`ChatGpt Output : ${answer}`);
-        res.send(answer);
-    }
-
-    apiCall();
+// Server Posting
+app.get('/studio/naming', async function (req, res) {
+    const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        max_tokens: 50,
+        temperature: 0.8, // 창의적인 답변을 얻기 위함
+        messages: [
+            {
+                role: 'system',
+                content:
+                    '당신의 최고의 프론트엔드 개발자입니다. 당신은 모든 프로그래밍 언어에 대한 코드 이해도가 아주 높으며, 리팩토링 또한 마틴 파울러처럼 잘 이해하고 있습니다. 만약 변수명에 대한 질문을 받는다면 해당 질문에 대한 올바른 답변을 할 수 있습니다 ',
+            },
+            {
+                role: 'user',
+                content:
+                    '당신의 최고의 프론트엔드 개발자입니다. 당신은 모든 프로그래밍 언어에 대한 코드 이해도가 아주 높으며, 리팩토링 또한 마틴 파울러처럼 잘 이해하고 있습니다. 만약 변수명에 대한 질문을 받는다면 해당 질문에 대한 올바른 답변을 할 수 있습니다 ',
+            },
+            {
+                role: 'assistant',
+                content:
+                    '안녕하세요 CodrLee 입니다. 보다 명확한 변수명, 함수명을 얻기위해 사용하고자 하는 로직이 있다면 구체적으로 알려주세요.',
+            },
+            {
+                role: 'user',
+                content: `현재 저는 버튼 인터렉션을 리액트 코드를 통해 짜고 있어요. 버튼에는 추가 삭제 수정 등의 기능이 첨부되어 있는데 이 로직에 맞는 변수명이 떠오르지 않습니다. 추천해줄 수 있나요?`,
+            },
+        ],
+    });
+    let writeVariable = completion.data.choices[0].message['content']; // content만 불러오기!
+    console.log(`ChatGpt Output : ${writeVariable}`);
+    res.send(writeVariable);
 });
 
 app.listen(PORT, () => {
